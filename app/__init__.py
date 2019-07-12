8d793097dfcc995b52da5e22c80fec6fedeb2d0f
newtfrank <mymbugua@gmail.com>
2019-07-12 11:41:08 +0300

    few changes

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,7 @@ from flask_pymongo import MongoClient
 import os
 
 # mongodb setup
-uri = 'mongodb+srv://creditor:creditor123@creditscore-fs5ta.gcp.mongodb.net/test?retryWrites=true&w=majority'S
+uri = 'mongodb+srv://creditor:creditor123@creditscore-fs5ta.gcp.mongodb.net/test?retryWrites=true&w=majority'
 client = MongoClient(uri, connectTimeoutMS=30000, connect=False)
 db = client.creditdb
 
